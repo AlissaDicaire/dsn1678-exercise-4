ebbc4e5418b4ae421bcc0e8bcc852b946ebe7608
Alissa Dicaire <dica0020@algonquinlive.com>
2014-10-07 10:43:43 -0400

    added css and js to final section

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var $dipperSection =$('.dipper-section');
 var $dipper =$('.dipper');
 var $shipSection =$('.ship-1-section');
 var $ship =$('.ship-1');
+var $ship2 =$('.ship-2'); 
+var $ship3Section =$('.ship-3-section');
 
 $win.on('scroll',function (){
 	var scrollPos = $win.scrollTop();
@@ -19,4 +21,8 @@ $dipperSection.waypoint(function () {
 
 $shipSection.waypoint(function () {
 	$ship.addClass('js-ship-fade');
+}, { offset: '50%' });
+
+$ship3Section.waypoint(function () {
+	$ship2.addClass('js-ship2-fade');
 }, { offset: '50%' });
