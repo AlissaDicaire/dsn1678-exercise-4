1dfab3d402300b1a17fc3d1280f09b47ba44f2ac
Alissa Dicaire <dica0020@algonquinlive.com>
2014-10-07 10:28:10 -0400

    added first shuttle and section second

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $win =$(window);
 var $sun =$('.sun');
 var $sunSection =$('.sun-section');
+var $dipperSection =$('.dipper-section');
+var $dipper =$('.dipper');
+var $shipSection =$('.ship-1-section');
+var $ship =$('.ship-1');
 
 $win.on('scroll',function (){
 	var scrollPos = $win.scrollTop();
@@ -8,3 +12,11 @@ $win.on('scroll',function (){
 	$sun.css('transform', 'rotate(' +scrollPos / 5 + 'deg)');
 	$sunSection.css('background-postion', 'center ' + scrollPos / 3 + 'px');		
 });
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' }); 
+
+$shipSection.waypoint(function () {
+	$ship.addClass('js-ship-fade');
+}, { offset: '50%' });
